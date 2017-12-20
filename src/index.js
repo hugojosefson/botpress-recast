e85d234d50af82ee4ed9607bc628c7ba5865905a
Hugo Josefson <hugo@josefson.org>
2017-12-20 12:11:02 +0100

    Add logging.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import R from 'ramda'
 import checkVersion from 'botpress-version-manager'
+import CircularJSON from 'circular-json'
 import Recast from './recast'
 import {byConfidence, renameSlugToValue} from './transform-intents'
 
@@ -7,12 +8,17 @@ let recast = null
 
 const incomingMiddleware = (event, next) => {
   if (event.type === 'message' || event.type === 'text') {
+    event.bp.logger.verbose(`[Recast.AI].incomingMiddleware: event = ${CircularJSON.stringify(event, null, 2)}`)
     if (event.bp.recast.mode === 'understanding') {
       Object.assign(recast.getUserContext(event.user.id).context, {
         botpress_platform: event.platform,
         botpress_type: event.type
       })
       recast.analyseText(event.user.id, event.text)
+        .then(result => {
+          event.bp.logger.verbose(`[Recast.AI].recast.analyseText: result = ${CircularJSON.stringify(result, null, 2)}`)
+          return result
+        })
         .then(({intents, entities, act, sentiment, language}) => {
           event.recast = {
             intents: R.compose(R.sort(byConfidence), R.map(renameSlugToValue), R.defaultTo([]))(intents),
@@ -22,6 +28,7 @@ const incomingMiddleware = (event, next) => {
             language,
             context: recast.getUserContext(event.user.id)
           }
+          event.bp.logger.verbose(`[Recast.AI].analyseText: event.recast = ${CircularJSON.stringify(event.recast, null, 2)}`)
           next()
         })
         .catch(err => next(err))
@@ -42,10 +49,12 @@ const incomingMiddleware = (event, next) => {
             replies: messages,
             context: recast.getUserContext(event.user.id)
           }
+          event.bp.logger.verbose(`[Recast.AI].converseText: event.recast = ${CircularJSON.stringify(event.recast, null, 2)}`)
         })
         .catch(err => next(err))
     }
   } else {
+    event.bp.logger.warn(`[Recast.AI].incomingMiddleware: event = ${CircularJSON.stringify(event, null, 2)}`)
     next()
   }
 }
@@ -67,10 +76,12 @@ export default {
   },
 
   init: (bp, configurator) => {
+    bp.logger.debug('init')
     checkVersion(bp, __dirname)
 
     recast = Recast(bp)
 
+    bp.logger.debug('Registering')
     bp.middlewares.register({
       name: 'recast.incoming',
       module: 'botpress-recast',
@@ -80,11 +91,13 @@ export default {
       description: 'Understands entities from incoming message and suggests or executes actions.'
     })
 
+    bp.logger.debug('Configuring')
     configurator.loadAll()
       .then(config => recast.setConfiguration(config))
   },
 
   ready: (bp, configurator) => {
+    bp.logger.debug('Ready')
     const router = bp.getRouter('botpress-recast')
 
     router.get('/config', (req, res) => {
