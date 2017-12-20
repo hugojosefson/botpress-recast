462feb15fb275ea5e87756e80c6e1cb932c942e3
Hugo Josefson <hugo@josefson.org>
2017-12-20 12:13:00 +0100

    Fix message parsing.

--- src/index.js
+++ src/index.js
@@ -6,26 +6,25 @@ import {byConfidence, renameSlugToValue} from './transform-intents'
 let recast = null
 
 const incomingMiddleware = (event, next) => {
-  if (event.type === 'message') {
+  if (event.type === 'message' || event.type === 'text') {
     if (event.bp.recast.mode === 'understanding') {
       Object.assign(recast.getUserContext(event.user.id).context, {
         botpress_platform: event.platform,
         botpress_type: event.type
       })
       recast.analyseText(event.user.id, event.text)
-                .then(({intents, entities, act, sentiment, language}) => {
-                  event.recast = {
-                    intents: R.compose(R.sort(byConfidence), R.map(renameSlugToValue), R.defaultTo([]))(intents),
-                    entities: R.compose(R.map(R.sort(byConfidence)), R.defaultTo({}))(entities),
-                    act,
-                    sentiment,
-                    language,
-                    context: recast.getUserContext(event.user.id)
-                  }
-                  event.bp.logger.verbose(`[Recast.AI].analyseText: event.recast = ${JSON.stringify(event.recast, null, 2)}`)
-                  next()
-                })
-                .catch(err => next(err))
+        .then(({intents, entities, act, sentiment, language}) => {
+          event.recast = {
+            intents: R.compose(R.sort(byConfidence), R.map(renameSlugToValue), R.defaultTo([]))(intents),
+            entities: R.compose(R.map(R.sort(byConfidence)), R.defaultTo({}))(entities),
+            act,
+            sentiment,
+            language,
+            context: recast.getUserContext(event.user.id)
+          }
+          next()
+        })
+        .catch(err => next(err))
     } else {
       Object.assign(recast.getUserContext(event.user.id).context, {
         botpress_platform: event.platform,
@@ -33,20 +32,18 @@ const incomingMiddleware = (event, next) => {
       })
 
       recast.converseText(event.user.id, event.text)
-                .then(({intents, entities, act, sentiment, language, action, replies}) => {
-                  event.recast = {
-                    intents: R.compose(R.sort(byConfidence), R.map(renameSlugToValue, R.defaultTo([])))(intents),
-                    entities: R.compose(R.map(R.sort(byConfidence)), R.defaultTo({}))(entities),
-                    act,
-                    sentiment,
-                    language,
-                    action,
-                    replies,
-                    context: recast.getUserContext(event.user.id)
-                  }
-                  event.bp.logger.verbose(`[Recast.AI].converseText: event.recast = ${JSON.stringify(event.recast, null, 2)}`)
-                })
-                .catch(err => next(err))
+        .then(({messages, nlp: {intents, entities, act, sentiment, language}}) => {
+          event.recast = {
+            intents: R.compose(R.sort(byConfidence), R.map(renameSlugToValue, R.defaultTo([])))(intents),
+            entities: R.compose(R.map(R.sort(byConfidence)), R.defaultTo({}))(entities),
+            act,
+            sentiment,
+            language,
+            replies: messages,
+            context: recast.getUserContext(event.user.id)
+          }
+        })
+        .catch(err => next(err))
     }
   } else {
     next()
@@ -84,7 +81,7 @@ export default {
     })
 
     configurator.loadAll()
-            .then(config => recast.setConfiguration(config))
+      .then(config => recast.setConfiguration(config))
   },
 
   ready: (bp, configurator) => {
@@ -92,16 +89,16 @@ export default {
 
     router.get('/config', (req, res) => {
       configurator.loadAll()
-                .then(c => res.send(c))
+        .then(c => res.send(c))
     })
 
     router.post('/config', (req, res) => {
       const {accessToken, selectedMode} = req.body
 
       configurator.saveAll({accessToken, selectedMode})
-                .then(() => configurator.loadAll())
-                .then(c => recast.setConfiguration(c))
-                .then(() => res.sendStatus(200))
+        .then(() => configurator.loadAll())
+        .then(c => recast.setConfiguration(c))
+        .then(() => res.sendStatus(200))
     })
   }
 }
